a459f097335c1c6b4eacd27e3a7d0b482af817ea
Masplus <masplus1@gmail.com>
2013-02-15 23:47:56 +0100

    Nova codificació mes exigent
    S'ha millorat el deploy.sh per a que realitzi les comprovacions
    necessàries per fer les còpies de seguretat i l'aplicació de les noves
    traduccions.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,46 @@
-#!/bin/sh
+#!/bin/bash
+if [ $(whoami) = "root" ]; then
+	echo -e -n "\n \n \n EXECUTING SCRIPT \n \n"	
 
-NATIVE2ASCII="/opt/zimbra/java/bin/native2ascii"
-DESTDIR="/opt/zimbra/jetty/webapps/zimbra/WEB-INF/classes/messages"
+	NATIVE2ASCII="/opt/zimbra/java/bin/native2ascii"
+	DESTDIR="/opt/zimbra/jetty/webapps/zimbra/WEB-INF/classes/messages"
+	DESTDIRADMIN="/opt/zimbra/jetty/webapps/zimbraAdmin/WEB-INF/classes/messages"
 
-$NATIVE2ASCII -encoding UTF-8 AjxMsg.properties AjxMsg_ca.properties
-$NATIVE2ASCII -encoding UTF-8 ZmMsg.properties ZmMsg_ca.properties
-$NATIVE2ASCII -encoding UTF-8 ZMsg.properties ZMsg_ca.properties
-$NATIVE2ASCII -encoding UTF-8 I18nMsg.properties I18nMsg_ca.properties
+	if [ -d $DESTDIR ] && [ -d $DESTDIRADMIN ]; then
+		echo -e -n "\t Checking directories  \n \t \t ${DESTDIR} \n \t \t ${DESTDIRADMIN} \n \t [OK!] \n"
+		echo -e -n "\t Starting backups \n"
 
-mkdir backups
-cp $DESTDIR/*_ca.properties backups/
+		NOW=$(date +"%Y_%m_%d_%H_%M")
+		
+		mkdir backup-${NOW}
+		mkdir backup-${NOW}/zimbra
+		mkdir backup-${NOW}/zimbraAdmin
 
-mv *_ca.properties $DESTDIR/
+		cp ${DESTDIR}/*_ca.properties backup-${NOW}/zimbra
+		cp ${DESTDIRADMIN}/*_ca.properties backup-${NOW}/zimbraAdmin
+
+		echo -e -n "\t Completed backups! \n"
+		echo -e -n "\t Encoding new files \n"
+
+		TRANSLATION_FILES="AjxMsg I18nMsg ZabMsg ZbMsg ZmMsg ZmSMS ZaMsg ZhMsg ZMsg" 
+
+		for n_prefix in ${TRANSLATION_FILES} ; do 
+
+			$NATIVE2ASCII -encoding UTF-8 ${n_prefix}.properties ${n_prefix}_ca.properties 
+		
+		done 
+
+		echo -e -n "\t Encoding complete \n"
+		echo -e -n "\t Copying new files \n"
+			cp -a *_ca.properties ${DESTDIR}/
+			cp -a *_ca.properties ${DESTDIRADMIN}/
+		echo -e -n "\t Copy done \n"
+		
+		else 
+			echo -e -n " \n Directory  ${DESTDIR} \n  OR  \n  ${DESTDIRADMIN}  \n Do not exists \n"
+	fi
+	echo -e -n "\n  SCRIPT DONE \n \n \n"	 
+else
+	echo -e -n " \n You must be user 'root' to run this script \n"
+	exit 1
+fi
